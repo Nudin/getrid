59de5eab46c29a6bea401289370f51cc8722052e
Michael F. Schönitzer <michael@schoenitzer.de>
2019-05-05 02:17:43 +0200

    Refactor

--- getrid.py
+++ getrid.py
@@ -9,8 +9,6 @@ from pacgraph import Arch, human_si, packs_by_size, toplevel_packs
 to_remove = []
 keep = []
 
-info = urwid.Text("")
-
 
 def get_package_list():
     arch = Arch()
@@ -21,73 +19,90 @@ def get_package_list():
     return strings
 
 
-def show_text(text):
-    info.set_text(text)
+def get_info(softwarename):
+    subp = subprocess.run(["pacman", "-Qi", softwarename], capture_output=True)
+    return subp.stdout
 
 
-def show_or_exit(key):
-    if key in ("q", "Q"):
-        raise urwid.ExitMainLoop()
-    selected_button = main.base_widget.get_focus_widgets()[0].base_widget
-    if repr(key) == "'right'":
-        softwarename = selected_button.label.split()[1]
-        subp = subprocess.run(["pacman", "-Qi", softwarename], capture_output=True)
-        show_text(subp.stdout)
-    elif repr(key) == "'h'":
-        softwarename = selected_button.label.split()[1]
-        keep_item(selected_button, selected_button.label)
+def hide(button, name):
+    if name in keep:
+        keep.remove(name)
+        button.set_state("")
     else:
-        show_text(repr(key))
+        keep.append(name)
+        button.set_state("keep")
+
+
+def on_click(button, choice):
+    size, name = choice.split()
+    if name in to_remove:
+        to_remove.remove(name)
+        button.set_state("")
+    else:
+        to_remove.append(name)
+        button.set_state("rm")
 
 
 def menu(title, choices):
     body = [urwid.Text(title), urwid.Divider()]
     for c in choices:
-        button = urwid.Button(c)
-        urwid.connect_signal(button, "click", item_chosen, c)
+        button = Button(c)
+        urwid.connect_signal(button, "click", on_click, c)
         body.append(urwid.AttrMap(button, None, focus_map="reversed"))
     return urwid.ListBox(urwid.SimpleFocusListWalker(body))
 
 
-def keep_item(button, choice):
-    size, name = choice.split()
-    if name in keep:
-        keep.remove(name)
-        button.set_label(("", choice))
-    else:
-        keep.append(name)
-        button.set_label(("keep", choice))
+class Button(urwid.Button):
+    def set_state(self, state):
+        self.set_label((state, self.label))
 
 
-def item_chosen(button, choice):
-    size, name = choice.split()
-    if name in to_remove:
-        to_remove.remove(name)
-        button.set_label(("", choice))
-    else:
-        to_remove.append(name)
-        button.set_label(("rm", choice))
+class Tui:
+    palette = [
+        ("reversed", "standout", ""),
+        ("rm", "black", "dark red"),
+        ("keep", "light gray", "dark blue"),
+    ]
+
+    def __init__(self):
+        self.main = urwid.Padding(menu("Title", get_package_list()), left=2, right=2)
+        self.info = urwid.Text("")
+        self.right = urwid.Filler(self.info, "top")
+        self.cols = urwid.Columns([self.main, self.right])
+        self.loop = urwid.MainLoop(
+            self.cols, palette=self.palette, unhandled_input=self.handle_input
+        )
+
+    def __run__(self, stdscr):
+        self.loop.run()
+
+    def run(self):
+        wrapper(self.__run__)
+
+    def get_selected(self):
+        return self.main.base_widget.get_focus_widgets()[0].base_widget
+
+    def get_selected_text(self):
+        return self.get_selected().label.split()[1]
+
+    def show_text(self, text):
+        self.info.set_text(text)
+
+    def exit(self):
+        raise urwid.ExitMainLoop()
+
+    def handle_input(self, key):
+        if key in ("q", "Q"):
+            tui.exit()
+
+        button = tui.get_selected()
+        name = tui.get_selected_text()
+        if key == "right":
+            self.show_text(get_info(name))
+        elif key == "h":
+            hide(button, name)
 
 
-def exit_program(button):
-    raise urwid.ExitMainLoop()
-
-
-palette = [
-    ("reversed", "standout", ""),
-    ("rm", "black", "dark red"),
-    ("keep", "light gray", "dark blue"),
-]
-
-main = urwid.Padding(menu("Title", get_package_list()), left=2, right=2)
-right = urwid.Filler(info, "top")
-cols = urwid.Columns([main, right])
-loop = urwid.MainLoop(cols, palette=palette, unhandled_input=show_or_exit)
-
-
-def run(stdscr):
-    loop.run()
-
-
-wrapper(run)
+tui = Tui()
+tui.run()
 print(to_remove)
